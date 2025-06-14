3507f844ad584d1192bbdc00dc736d06dbe3ecbb
mckeem7768 <mckeem7768@student.faytechcc.edu>
2025-06-14 19:49:28 -0400

    Add files via upload
    P1Lab1 submission CTI110 for P1Lab2

---
+++ P1LAB1_MckeeMathew.py
@@ -0,0 +1,10 @@
+# Mathew McKee
+# 6/14/2025
+# P1LAB1
+# input from user and display output
+
+print("Enter your first name:", input())
+first_name=input()
+print("Enter your last name:", input())
+last_name=input()
+print("Hello,", first_name, last_name, "welcome to CTI-110." )
