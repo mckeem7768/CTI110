dc059d96a6f435586cf21501a73194bfa6f9c2ca
mckeem7768 <mckeem7768@student.faytechcc.edu>
2025-06-15 03:46:03 +0000

    P1HW1 push to main

---
+++ P1HW1_MckeeMathew.py
@@ -0,0 +1,16 @@
+# Mathew McKee
+# 6/14/2025
+# P1HW1
+# calculations of input integers
+
+print("-----Calculating Exponents----\n")
+base = int(input("Enter an integer as the base value: "))
+exp = int(input("Enter an integer as the exponent: "))
+print()
+print(base, "raised to the power of", exp, "is", int(base**exp), "!!", "\n")
+print("-----Addition and Subtraction----\n")
+start = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: "))
+sub = int(input("Enter an integer to subtract: "))
+print()
+print(start, "+", add, "-", sub, "is equal to", int(start+add-sub))
