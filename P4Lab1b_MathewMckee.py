aebcc9fe6bba0973cb022b3020145b01408bb291
mckeem7768 <mckeem7768@student.faytechcc.edu>
2025-07-05 17:34:02 -0400

    Add files via upload
    python turtle initials

---
+++ P4Lab1b_MathewMckee.py
@@ -0,0 +1,30 @@
+# Mathew McKee
+# 7/5/2025
+# P4Lab1b
+# Turtle draw initials
+
+import turtle
+
+# Initial first
+turtle.left(90)
+turtle.forward(60)
+turtle.right(135)
+turtle.forward(45)
+turtle.left(90)
+turtle.forward(45)
+turtle.right(135)
+turtle.forward(60)
+# Space
+turtle.penup()
+turtle.left(90)
+turtle.forward(10)
+turtle.pendown()
+# Initial
+turtle.left(90)
+turtle.forward(60)
+turtle.right(135)
+turtle.forward(45)
+turtle.left(90)
+turtle.forward(45)
+turtle.right(135)
+turtle.forward(60)
